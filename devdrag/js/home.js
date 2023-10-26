7a59e54c756de5bbb896b956adb34ec487786df5
triptimall <148651210+triptimall@users.noreply.github.com>
2023-10-26 18:14:54 +0530

    Add files via upload

---
+++ devdrag/js/home.js
@@ -0,0 +1,119 @@
+// {/* <script>
+//   const goToPage1Button = document.getElementById('goToPage1');
+//   const goToPage3Button = document.getElementById('goToPage3');
+
+//   goToPage1Button.addEventListener('click', function() {
+//     // Navigate to page1.html
+//     window.location.href = 'page1.html';
+//   });
+
+//   goToPage3Button.addEventListener('click', function() {
+//     // Navigate to page3.html
+//     window.location.href = 'page3.html';
+//   });
+// </script> */}
+
+
+//         const goToPage1Button = document.getElementById('homeBtn');
+//         const goToPage3Button = document.getElementById('goToPage3');
+      
+//         goToPage1Button.addEventListener('click', function() {
+         
+//           window.location.href = './images/work/html/home.html';
+//         });
+      
+//         goToPage3Button.addEventListener('click', function() {
+          
+//           window.location.href = 'page3.html';
+//         });
+    
+
+
+//     const goToPage2Button= document.getElementById('aboutBtn');
+//     const goToPage4Button = document.getElementById('goToPage4');
+  
+//     goToPage2Button.addEventListener('click', function() {
+//       // Navigate to page1.html
+//       window.location.href = './images/work/html/about.html';
+//     });
+  
+//     goToPage4Button.addEventListener('click', function() {
+//       // Navigate to page3.html
+//       window.location.href = 'page4.html';
+//     });
+
+
+
+//     const goToPage5Button= document.getElementById('OurProcessBtn');
+//     const goToPage6Button = document.getElementById('goToPage6');
+  
+//     goToPage5Button.addEventListener('click', function() {
+//       // Navigate to page1.html
+//       window.location.href = './images/work/html/OurProcess.html';
+//     });
+  
+//     goToPage6Button.addEventListener('click', function() {
+//       // Navigate to page3.html
+//       window.location.href = 'page6.html';
+//     });
+ 
+
+
+//     const goToPage7Button= document.getElementById('ServicesBtn');
+//     const goToPage8Button = document.getElementById('goToPage8');
+  
+//     goToPage7Button.addEventListener('click', function() {
+//       // Navigate to page1.html
+//       window.location.href = './images/work/html/Services.html';
+//     });
+  
+//     goToPage8Button.addEventListener('click', function() {
+//       // Navigate to page3.html
+//       window.location.href = 'page8.html';
+//     });
+ 
+
+
+//     const goToPage9Button= document.getElementById('OurWorkBtn');
+//     const goToPage10Button = document.getElementById('goToPage10');
+  
+//     goToPage9Button.addEventListener('click', function() {
+//       // Navigate to page1.html
+//       window.location.href = './images/work/html/OurWork.html';
+//     });
+  
+//     goToPage10Button.addEventListener('click', function() {
+//       // Navigate to page3.html
+//       window.location.href = 'page10.html';
+//     });
+
+
+
+//     const goToPage11Button= document.getElementById('PricingBtn');
+//     const goToPage12Button = document.getElementById('goToPage12');
+  
+//     goToPage11Button.addEventListener('click', function() {
+//       // Navigate to page1.html
+//       window.location.href = './images/work/html/Pricing.html';
+//     });
+  
+//     goToPage12Button.addEventListener('click', function() {
+//       // Navigate to page3.html
+//       window.location.href = 'page12.html';
+//     });
+ 
+
+
+//     const goToPage13Button= document.getElementById('ContactBtn');
+//     const goToPage14Button = document.getElementById('goToPage14');
+  
+//     goToPage13Button.addEventListener('click', function() {
+//       // Navigate to page1.html
+//       window.location.href = './images/work/html/Contact.html';
+//     });
+  
+//     goToPage14Button.addEventListener('click', function() {
+//       // Navigate to page3.html
+//       window.location.href = 'page14.html';
+//     });
+ 
---
+++ devdrag/js/sidebar.js
@@ -0,0 +1,43 @@
+//jquery-click-scroll
+//by syamsul'isul' Arifin
+
+// var sectionArray = [1, 2, 3, 4, 5, 6, 7];
+
+// $.each(sectionArray, function(index, value){
+          
+//      $(document).scroll(function(){
+//          var offsetSection = $('#' + 'section_' + value).offset().top - 80;
+//          var docScroll = $(document).scrollTop();
+//          var docScroll1 = docScroll + 1;
+         
+        
+//          if ( docScroll1 >= offsetSection ){
+//              $('.sidebar-side .sidebar-item .sidebar-link').removeClass('active');
+//              $('.sidebar-side .sidebar-item .sidebar-link:link').addClass('inactive');  
+//              $('.sidebar-side .sidebar-item .sidebar-link').eq(index).addClass('active');
+//              $('.sidebar-side .sidebar-item .sidebar-link').eq(index).removeClass('inactive');
+//          }
+         
+//      });
+    
+//     $('.sidebar-side .sidebar-item .sidebar-link').eq(index).click(function(e){
+//         var offsetClick = $('#' + 'section_' + value).offset().top - 80;
+//         e.preventDefault();
+//         $('html, body').animate({
+//             'scrollTop':offsetClick
+//         }, 300)
+        
+        
+//     });
+ 
+    
+// });
+
+// $(document).ready(function(){
+//     $('.sidebar-side .sidebar-item .sidebar-link:link').addClass('inactive');    
+//     $('.sidebar-side .sidebar-item .sidebar-link').eq(0).addClass('active');
+//     $('.sidebar-side .sidebar-item .sidebar-link:link').eq(0).removeClass('inactive');
+    
+
+
+// });
